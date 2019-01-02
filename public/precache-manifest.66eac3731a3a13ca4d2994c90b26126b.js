828f97aeaf551bba268fcbe06353b263472e8a01
webmcki <webmcki@gmail.com>
2019-01-02 15:51:50 +0900

    배포준비

---
+++ public/precache-manifest.66eac3731a3a13ca4d2994c90b26126b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ec7f71cf99891b3b86f",
+    "url": "/static/css/main.42ab7db6.chunk.css"
+  },
+  {
+    "revision": "7ec7f71cf99891b3b86f",
+    "url": "/static/js/main.7ec7f71c.chunk.js"
+  },
+  {
+    "revision": "675353bbabbb2afdc4c1",
+    "url": "/static/js/1.675353bb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "154191be7aa56bbe2dcfcbea9a06e287",
+    "url": "/static/media/logo.154191be.jpg"
+  },
+  {
+    "revision": "a8ccb783e31dcf8007706f92d1eabc29",
+    "url": "/index.html"
+  }
+];
